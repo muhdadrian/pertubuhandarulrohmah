432e97850eb08df67b81b29861d8041ca6a472f6
Adrian Nandu <adriannandu@kingdeMacBook-Air.local>
2023-03-09 00:26:11 +0800

    front

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
 'use strict';
 
-//
+//first section
+
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+});
+
+// second section
+
+const hamburgerMenu = document.querySelector('.hamburger');
+
+document.addEventListener('click', function (e) {
+  if (!hamburger.contains(e.target) && !navMenu.contains(e.target)) {
+    hamburger.classList.remove('active');
+    navMenu.classList.remove('active');
+  }
+});
